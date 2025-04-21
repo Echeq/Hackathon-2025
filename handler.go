da6a22c49410df5508e299acffca0248b5f65682
MemerZxZ <135417441+MemerZxZ@users.noreply.github.com>
2025-04-21 15:59:07 +0800

    I TRIED AAAAAAAAAA
    try to implement the HTTP shit, but the red can't go away, IT HAUNTS MEEEEEEE. alr asked like 4 fockin AI, but it all comes out red.

--- handler.go
+++ handler.go
@@ -1,15 +1,86 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"context"
-	example "kitex-multi-protocol/kitex_gen/hello/example"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/cloudwego/kitex/pkg/remote"
 )
 
-// HelloServiceImpl implements the last service interface defined in the IDL.
-type HelloServiceImpl struct{}
+// DualProtocolHandler supports HTTP and Thrift traffic on one port
+type DualProtocolHandler struct {
+	delegate    remote.ServerTransHandler
+	httpHandler http.Handler
+}
 
-// HelloMethod implements the HelloServiceImpl interface.
-func (s *HelloServiceImpl) HelloMethod(ctx context.Context, request *example.HelloReq) (resp *example.HelloResp, err error) {
-	// TODO: Your code here...
-	return
+func NewDualProtocolHandler(delegate remote.TransHandler, httpHandler http.Handler) *DualProtocolHandler {
+	return &DualProtocolHandler{
+		delegate:    delegate,
+		httpHandler: httpHandler,
+	}
+}
+
+func (h *DualProtocolHandler) OnRead(ctx context.Context, conn net.Conn) error {
+	buf := make([]byte, 4)
+	_, err := conn.Read(buf)
+	if err != nil {
+		return err
+	}
+	conn = &peekedConn{Conn: conn, peeked: buf}
+
+	if isHTTPRequest(buf) {
+		return h.handleHTTP(conn)
+	}
+	return h.delegate.OnRead(ctx, conn)
+}
+
+func (h *DualProtocolHandler) OnActive(ctx context.Context, conn net.Conn) error {
+	return h.delegate.OnActive(ctx, conn)
+}
+
+func (h *DualProtocolHandler) OnInactive(ctx context.Context, conn net.Conn) error {
+	return h.delegate.OnInactive(ctx, conn)
+}
+
+func (h *DualProtocolHandler) OnError(ctx context.Context, err error, conn net.Conn) {
+	h.delegate.OnError(ctx, err, conn)
+}
+
+func isHTTPRequest(buf []byte) bool {
+	return bytes.HasPrefix(buf, []byte("GET ")) ||
+		bytes.HasPrefix(buf, []byte("POST")) ||
+		bytes.HasPrefix(buf, []byte("PUT ")) ||
+		bytes.HasPrefix(buf, []byte("HTTP"))
+}
+
+func (h *DualProtocolHandler) handleHTTP(conn net.Conn) error {
+	req, err := http.ReadRequest(bufio.NewReader(conn))
+	if err != nil {
+		return err
+	}
+	defer req.Body.Close()
+
+	response := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n%s",
+		`{"message":"hello from server"}`)
+
+	_, err = conn.Write([]byte(response))
+	return err
+}
+
+type peekedConn struct {
+	net.Conn
+	peeked []byte
+}
+
+func (c *peekedConn) Read(p []byte) (n int, err error) {
+	if len(c.peeked) > 0 {
+		n = copy(p, c.peeked)
+		c.peeked = c.peeked[n:]
+		return n, nil
+	}
+	return c.Conn.Read(p)
 }
--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
-	example "kitex-multi-protocol/kitex_gen/hello/example/helloservice"
 	"log"
+	"net/http"
+
+	example "kitex-multi-protocol/kitex_gen/hello/example/helloservice"
+
+	"github.com/cloudwego/kitex/pkg/remote"
+	"github.com/cloudwego/kitex/server"
 )
 
 func main() {
-	svr := example.NewServer(new(HelloServiceImpl))
+	// Construct internal thrift handler
+	thriftHandler := remote.NewTransServerHandler(remote.ServerOption{})
 
-	err := svr.Run()
+	// Construct dual handler with HTTP and thrift support
+	dualHandler := NewDualProtocolHandler(thriftHandler, http.NewServeMux()) // httpHandler can be customized
 
-	if err != nil {
+	// Create the Kitex server and pass the custom transport handler
+	svr := example.NewServer(
+		new(HelloServiceImpl),
+		server.WithTransHandler(dualHandler),
+	)
+
+	if err := svr.Run(); err != nil {
 		log.Println(err.Error())
 	}
 }
